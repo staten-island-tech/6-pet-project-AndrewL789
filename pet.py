3bcb1224a83c01f50c907fcd6b2d9847042667e4
AndrewL789 <Andrewl434@nycstudents.net>
2025-11-03 09:26:14 -0500

    grah grah grah

--- pet.py
+++ pet.py
@@ -1,31 +1,60 @@
-""" 
+
 # hero oactivity 
+shop = [
+    {
+        'name':'sword',
+        'price': 2.99
+    }
+]
+
 class Hero:
     def __init__(self, name, money, status, inv):
         self.name = name
-        self.money = money
+        self.__money = money
         self.inv = inv
         self.status = status
     def buy(self, item):
-        (self.inv).append(item)
-        print(self.inv)
+        for grah in shop:
+            if item == grah['name']:
+                self.inv.append(item)
+                self.__money -= grah['price'] 
+                print(self.inv) 
+                print(self.__money)
 
-yangxi = Hero("YangXi", 3.99, ["Bone Cancer"], ["dirty used syringe"])
-print(yangxi.inv)
-yangxi.buy('air') """
+
+
+#yangxi = Hero("YangXi", 3.99, ["Bone Cancer"], ["dirty used syringe"])
+#print(yangxi.inv)
+#yangxi.buy('sword')
 
 
 
 #pet
 
-class pet:
+""" class pet:
     def __init__(self, name, hapiness):
         self.name = name
         self.__hapiness = hapiness
     def play(self, thingy):
         self.__hapiness += 10
-        print(f'hapiness is now {self.__hapiness} after playing {thingy}')
+        print(f"{self.name}'s hapiness is now {self.__hapiness} after playing {thingy}")
+
+ """
+#yang = pet('Yang', 0) 
+#yang.play("yang game") 
 
 
-yang = pet('Yang', 0) 
-yang.play("yang game") 
+
+
+class petpetpet:
+    def __init__(self, name, hapiness):
+        self.name = name
+        self.__hapiness = hapiness
+    def play(self, action):
+        p = 10
+        self.__hapiness += p
+        print(f"{self.name} is {action}, his joy has surged by {p}!!!!")
+        
+yang = petpetpet('XiYang', 50) 
+yang.play("xiyang'in") 
+
