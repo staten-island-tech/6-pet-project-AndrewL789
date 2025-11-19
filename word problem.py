e202415bd39308bc799017d9a66eab159365def9
AndrewL789 <Andrewl434@nycstudents.net>
2025-11-19 09:24:15 -0500

    Huzzah

--- word problem.py
+++ word problem.py
@@ -1,16 +1,26 @@
 def valid(email, password):
+    if type(email) != str or type(password) != str:
+        return "Error: Email or password not a string"
     email.strip
     if '@' not in email:
         return "Error: Not a valid email format"
-    capital = False
+    capital = False 
     numero = False
     for i in password:
         if i == i.upper():
             capital = True
     for i in password:
-        if i == i.isdigit():
+        if i.isdigit():
             numero = True
-    if capital == False
-valid('doodoo@',"paswOrdddddd2")
+    if capital == False:
+        return 'Error: Needs a capital'
+    if numero == False:
+        return 'Error: Needs a number'
+    if len(password) < 8:
+        return 'Error: To short'
+    return{'email':email, 'password':password}
+print(valid('doodoo@babayaga.com',"pAssw0rDelam&ndo"))
+
+
 
                     
